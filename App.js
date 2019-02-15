4e0759c138f09cc8784d105c8ca6cba4bca2f709
Daniel Dimitrov <daniel@compojoom.com>
2019-02-15 09:31:00 +0100

    same bug with gesture handler

--- App.js
+++ App.js
@@ -8,70 +8,54 @@
  */
 
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Animated } from 'react-native'
+
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { PanGestureHandler, } from 'react-native-gesture-handler';
 
-class HomeScreen extends React.Component {
-  static navigationOptions = ({navigation}) => {
-    return {
-      title: 'Header',
-      // headerLeft: <HeaderCancel/>,
-      headerRight: <TouchableOpacity>
-        <Text style={{color: 'red'}}>Done</Text>
-      </TouchableOpacity>
-    }
-  }
 
-  render () {
+export default class Example extends Component {
+  render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Move marker, then press on the Done button in the header. It won't work. One needs to move the map
-        for the buttons to become clickable again.
-        </Text>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
+      <View style={styles.scrollView}>
+        <PanGestureHandler enabled={false}
         >
-          <Marker coordinate={{latitude: 45, longitude: 8}} draggable={true}/>
-        </MapView>
+          <Animated.View
+            style={{flex: 1, backgroundColor: '#fff'}}
+          >
+            <MapView
+              provider={PROVIDER_GOOGLE}
+              style={styles.map}
+            >
+              <Marker coordinate={{latitude: 45, longitude: 8}} draggable={true}/>
+            </MapView>
+          </Animated.View>
+        </PanGestureHandler>
 
-        <TouchableOpacity>
-          <Text>this button can't be pressed as well when marker moved</Text>
+        <TouchableOpacity style={{height: 50, backgroundColor: 'yellow', justifyContent: 'center', alignItems: 'center'}}>
+          <Text>press me</Text>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-})
-
-export default createAppContainer(AppNavigator)
-
 const styles = StyleSheet.create({
-  map: {
+  scrollView: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  box: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    backgroundColor: 'plum',
+    margin: 10,
+    zIndex: 200,
+  },
+    map: {
     width: 500,
     height: 500
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-})
+});
